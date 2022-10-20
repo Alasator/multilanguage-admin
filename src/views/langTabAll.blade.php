f78d8132654e491c516cde1840113c781c2550b1
Ark <grigorets.arkadiy@gmail.com>
2022-10-20 19:14:53 +0300

    update template

--- src/views/langTabAll.blade.php
+++ src/views/langTabAll.blade.php
@@ -28,7 +28,8 @@
             @foreach($forms as $pk => $form)
                 <li class="@if ($form == reset($forms)) active @endif ">
                     <a href="#{{ str_replace('.', '-', $relationName) . '_' . $pk }}" data-toggle="tab">
-                        {{ config('translatable.native_locale.' . $pk, $pk) }} <i class="fa fa-exclamation-circle text-red hide"></i>
+                        {{ config('translatable.native_locale.' . $pk, $pk) }} <i
+                                class="fa fa-exclamation-circle text-red hide"></i>
                     </a>
                 </li>
             @endforeach
@@ -41,18 +42,21 @@
                             $language_index = $key . '-' . $national;
                         @endphp
                         <li class="@if ($locale == config('app.locale')) active @endif">
-                            <a href="#{{ str_replace('.', '-', $relationName) . '_' . $language_index }}" data-toggle="tab">
-                                {{ config('translatable.native_locale.' . $language_index, $language_index) }} <i class="fa fa-exclamation-circle text-red hide"></i>
+                            <a href="#{{ str_replace('.', '-', $relationName) . '_' . $language_index }}"
+                               data-toggle="tab">
+                                {{ config('translatable.native_locale.' . $language_index, $language_index) }} <i
+                                        class="fa fa-exclamation-circle text-red hide"></i>
                             </a>
                         </li>
-                        @endforeach
-                    @else
+                    @endforeach
+                @else
                     <li class="@if ($locale == config('laravellocalization.localesOrder')[0]) active @endif">
                         <a href="#{{ str_replace('.', '-', $relationName) . '_' . $locale }}" data-toggle="tab">
-                            {{ config('translatable.native_locale.' . $locale, $locale) }} <i class="fa fa-exclamation-circle text-red hide"></i>
+                            {{ config('translatable.native_locale.' . $locale, $locale) }} <i
+                                    class="fa fa-exclamation-circle text-red hide"></i>
                         </a>
                     </li>
-                    @endif
+                @endif
 
             @endforeach
         @endif
@@ -70,13 +74,15 @@
                 </div>
             @endforeach
         @else
-            @foreach(config('translatable.locales') as $key => $locale)
+            @foreach(config('laravellocalization.localesOrder') as $key => $locale)
                 @if(is_array($locale))
                     @foreach($locale as $national)
                         @php
                             $language_index = $key . '-' . $national;
                         @endphp
-                        <div class="tab-pane fields-group has-many-{{$column}}-form @if ($language_index == config('app.locale')) active @endif" id="{{ str_replace('.', '-', $relationName) . '_' . $language_index }}">
+                        <div
+                                class="tab-pane fields-group has-many-{{$column}}-form @if ($language_index == config('app.locale')) active @endif"
+                                id="{{ str_replace('.', '-', $relationName) . '_' . $language_index }}">
                             @foreach($template_fields as $field)
                                 @php
                                     $field->setElementName($column.'['. $language_index .'][' .$field->column() .']');
@@ -88,24 +94,27 @@
                                 @endphp
                                 {!! $field->render() !!}
                             @endforeach
-                            <input type="hidden" name="{{$relationName}}[{{ $language_index }}][loc]" value ="{{ $language_index }}">
+                            <input type="hidden" name="{{$relationName}}[{{ $language_index }}][loc]"
+                                   value="{{ $language_index }}">
                         </div>
                     @endforeach
 
-                    @else
-                    <div class="tab-pane fields-group has-many-{{$column}}-form @if ($locale == config('app.locale')) active @endif" id="{{ str_replace('.', '-', $relationName) . '_' . $locale }}">
+                @else
+                    <div
+                            class="tab-pane fields-group has-many-{{$column}}-form @if ($locale == config('laravellocalization.localesOrder')[0]) active @endif"
+                            id="{{ str_replace('.', '-', $relationName) . '_' . config('laravellocalization.localesOrder')[$key] }}">
                         @foreach($template_fields as $field)
                             @php
-                                $field->setElementName($column.'['. $locale .'][' .$field->column() .']');
-                                $field->attribute('name',$column.'['. $locale .'][' .$field->column() .']');
+                                $field->setElementName($column.'['. config('laravellocalization.localesOrder')[$key] .'][' .$field->column() .']');
+                                $field->attribute('name',$column.'['. config('laravellocalization.localesOrder')[$key] .'][' .$field->column() .']');
                                 $field->attribute('title',$field->column());
                                 if($field->column() == 'locale'){
-                                    $field->value($locale);
+                                    $field->value(config('laravellocalization.localesOrder')[$key]);
                                 }
                             @endphp
                             {!! $field->render() !!}
                         @endforeach
-                        <input type="hidden" name="{{$relationName}}[{{ $locale }}][loc]" value ="{{ $locale }}">
+                        <input type="hidden" name="{{$relationName}}[{{ config('laravellocalization.localesOrder')[$key] }}][loc]" value="{{ config('laravellocalization.localesOrder')[$key] }}">
                     </div>
                 @endif
 
@@ -118,7 +127,7 @@
             <a href="#{{ $relationName . '_new_' . \Encore\Admin\Form\NestedForm::DEFAULT_KEY_NAME }}"
                data-toggle="tab">
                 &nbsp;New {{ \Encore\Admin\Form\NestedForm::DEFAULT_KEY_NAME }} <i
-                    class="fa fa-exclamation-circle text-red hide"></i>
+                        class="fa fa-exclamation-circle text-red hide"></i>
             </a>
             <i class="close-tab fa fa-times"></i>
         </li>
